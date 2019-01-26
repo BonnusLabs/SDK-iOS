356f30dc360d52df00f890327f82d1ca5caca353
Emmanuel Casañas Cruz <emmanuel.c19@gmail.com>
2019-01-26 00:29:35 -0600

    Version 1.3
    Updated version for SDK 1.3 and Demo version

--- BonnusSDK.framework/Headers/BonnusSDK.h
+++ BonnusSDK.framework/Headers/BonnusSDK.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double BonnusSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char BonnusSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BonnusSDK/PublicHeader.h>
-
-
--- Testing/DemoViewController.swift
+++ Testing/DemoViewController.swift
@@ -33,9 +33,9 @@ class DemoViewController: UIViewController {
     }
     
     @IBAction func clickConfigSDK(_ sender: Any) {
-
-        Bonnus.instance.initWithCredentials(partnerID: "a3B7CdNShdCO4KzYrdZYtWwEEfr17J99",
-                                            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qUTFNVVJEUVVWRlEwUXpRekl3TXpNek9EVkROa1ZHUkRKRk16Y3lSREU1UWtNelJrTkNRUSJ9.eyJpc3MiOiJodHRwczovL3ZhbG1hbnMuYXV0aDAuY29tLyIsInN1YiI6InhTYzdraFBPdE8xMFMzUG0xMlFTRHM4R3JvOVNuZWJnQGNsaWVudHMiLCJhdWQiOiJodHRwOi8vYm9ubnVzYXBpLmF6dXJld2Vic2l0ZXMubmV0L2FwaSIsImlhdCI6MTU0MjA4MDQ2MSwiZXhwIjoxNTU3NjMyNDYxLCJhenAiOiJ4U2M3a2hQT3RPMTBTM1BtMTJRU0RzOEdybzlTbmViZyIsInNjb3BlIjoib3BlbmlkOnByb2ZpbGU6ZW1haWwgb3BlbmlkIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.lI34dkc99ruNoDxwN6nRbeyyjW5lxJWcsWLWO8D1nTHPhB_dNb0Af5dfknwNJoaxgR059IQ8XNLCku357lpiXDb-UVmdcoBegKTFp54N0PSVs5T1su1Rd5fbnrBTQX_T298zvnPph6-hzdCqPP_QDE_pyzEtNbhTXRx3FQQj9pRu7w7h-t-JiH_d9444075CF3mUngnIXW091oMZ9XSk6xeWAQRI5-AYuYzV3vsCos0mzPEJdXgtYAsNK3qcX-iwl8WxXlw-rWpK7DAZBdd03_Xqmhe8aRa5G7D1XjGzbUYowMhau2Yur_ZHH73RY-slQ-NGDSDgYbrrwx00wKZz4g", sdkId: "cec4c4ab490c4ef4a6d0d7c435ac9515",userID: "Swift-iOS")
+        
+        Bonnus.instance.initWithCredentials(partnerID: "d6SfD0X7WSnG247v7INpHPq7vxHJBvai",
+                                            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qUTFNVVJEUVVWRlEwUXpRekl3TXpNek9EVkROa1ZHUkRKRk16Y3lSREU1UWtNelJrTkNRUSJ9.eyJpc3MiOiJodHRwczovL3ZhbG1hbnMuYXV0aDAuY29tLyIsInN1YiI6InhTYzdraFBPdE8xMFMzUG0xMlFTRHM4R3JvOVNuZWJnQGNsaWVudHMiLCJhdWQiOiJodHRwOi8vYm9ubnVzYXBpLmF6dXJld2Vic2l0ZXMubmV0L2FwaSIsImlhdCI6MTU0MjA4MDQ2MSwiZXhwIjoxNTU3NjMyNDYxLCJhenAiOiJ4U2M3a2hQT3RPMTBTM1BtMTJRU0RzOEdybzlTbmViZyIsInNjb3BlIjoib3BlbmlkOnByb2ZpbGU6ZW1haWwgb3BlbmlkIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.lI34dkc99ruNoDxwN6nRbeyyjW5lxJWcsWLWO8D1nTHPhB_dNb0Af5dfknwNJoaxgR059IQ8XNLCku357lpiXDb-UVmdcoBegKTFp54N0PSVs5T1su1Rd5fbnrBTQX_T298zvnPph6-hzdCqPP_QDE_pyzEtNbhTXRx3FQQj9pRu7w7h-t-JiH_d9444075CF3mUngnIXW091oMZ9XSk6xeWAQRI5-AYuYzV3vsCos0mzPEJdXgtYAsNK3qcX-iwl8WxXlw-rWpK7DAZBdd03_Xqmhe8aRa5G7D1XjGzbUYowMhau2Yur_ZHH73RY-slQ-NGDSDgYbrrwx00wKZz4g", sdkId: "37i8rehebptnrmwxy0mfgmc2hyol4yjogu",userID: "testios")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(3000)) {
             self.checkStatusForSDK()
