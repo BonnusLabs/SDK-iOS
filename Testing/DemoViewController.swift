18feef0b77d052971b304f302de97647840a063b
Emmanuel Casañas <emmanuel.c19@gmail.com>
2019-05-16 15:29:11 -0500

    Delegate Method Implemented

--- Testing/DemoViewController.swift
+++ Testing/DemoViewController.swift
@@ -24,6 +24,7 @@ class DemoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         checkStatusForSDK()
+        Bonnus.instance.setCuponDelegate(delegate: self)
 
     }
 
@@ -34,8 +35,8 @@ class DemoViewController: UIViewController {
     
     @IBAction func clickConfigSDK(_ sender: Any) {
         
-        Bonnus.instance.initWithCredentials(partnerID: "d6SfD0X7WSnG247v7INpHPq7vxHJBvai",
-                                            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qUTFNVVJEUVVWRlEwUXpRekl3TXpNek9EVkROa1ZHUkRKRk16Y3lSREU1UWtNelJrTkNRUSJ9.eyJpc3MiOiJodHRwczovL3ZhbG1hbnMuYXV0aDAuY29tLyIsInN1YiI6InhTYzdraFBPdE8xMFMzUG0xMlFTRHM4R3JvOVNuZWJnQGNsaWVudHMiLCJhdWQiOiJodHRwOi8vYm9ubnVzYXBpLmF6dXJld2Vic2l0ZXMubmV0L2FwaSIsImlhdCI6MTU0MjA4MDQ2MSwiZXhwIjoxNTU3NjMyNDYxLCJhenAiOiJ4U2M3a2hQT3RPMTBTM1BtMTJRU0RzOEdybzlTbmViZyIsInNjb3BlIjoib3BlbmlkOnByb2ZpbGU6ZW1haWwgb3BlbmlkIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.lI34dkc99ruNoDxwN6nRbeyyjW5lxJWcsWLWO8D1nTHPhB_dNb0Af5dfknwNJoaxgR059IQ8XNLCku357lpiXDb-UVmdcoBegKTFp54N0PSVs5T1su1Rd5fbnrBTQX_T298zvnPph6-hzdCqPP_QDE_pyzEtNbhTXRx3FQQj9pRu7w7h-t-JiH_d9444075CF3mUngnIXW091oMZ9XSk6xeWAQRI5-AYuYzV3vsCos0mzPEJdXgtYAsNK3qcX-iwl8WxXlw-rWpK7DAZBdd03_Xqmhe8aRa5G7D1XjGzbUYowMhau2Yur_ZHH73RY-slQ-NGDSDgYbrrwx00wKZz4g", sdkId: "37i8rehebptnrmwxy0mfgmc2hyol4yjogu",userID: "testios")
+        Bonnus.instance.initWithCredentials(partnerID: "BH9biBeZIWOf9T28lCiyNklRCsKiiEYG",
+                                            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qUTFNVVJEUVVWRlEwUXpRekl3TXpNek9EVkROa1ZHUkRKRk16Y3lSREU1UWtNelJrTkNRUSJ9.eyJpc3MiOiJodHRwczovL3ZhbG1hbnMuYXV0aDAuY29tLyIsInN1YiI6IkJIOWJpQmVaSVdPZjlUMjhsQ2l5TmtsUkNzS2lpRVlHQGNsaWVudHMiLCJhdWQiOiJodHRwOi8vYm9ubnVzYXBpLmF6dXJld2Vic2l0ZXMubmV0L2FwaSIsImlhdCI6MTU0ODI1NzYyNywiZXhwIjoxNTYzODA5NjI3LCJhenAiOiJCSDliaUJlWklXT2Y5VDI4bENpeU5rbFJDc0tpaUVZRyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.r5aCq83GyqIdBGe9f09DbFmBu1ao6vM3ZNg35_fs6qm65kcsh3yPftrwPYM7zVxfTiBWBh3cgZeIOiy0wexnrR6_BV-_vU2xEopOW4Vb7DnWKlI8j-Owjqqhg8unT5mAewrn1QrHtJpG-hq4HOAJA2IB_OWxiBnQnvh83YOsMCPWhakyjviBkQhDnE81X023z20mpE_jYa0xbq2Yl6K6xbbYyXNRFTvh3taoND5IwA7oPI3lnH6AmC547qZjJSJZHM6bVyC1oAS-YnAT1aba6Fp0Ow609wy7oBdSA3aMrmZky53ShbP3hr22dloyLZZ-8tyl-0wJYIFWRk3bmgc6zQ", sdkId: "6a9cd43fb297418080d7da5a0b3afa2f",userID: "jobelle103")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(3000)) {
             self.checkStatusForSDK()
@@ -49,23 +50,23 @@ class DemoViewController: UIViewController {
     }
 
     @IBAction func ClickButtonOne(_ sender: Any) {
-        Bonnus.instance.tryToRedeemPrice(momento: "Pago de servicios")
+        Bonnus.instance.tryToRedeemPrice(momento: "Momento1")
     }
 
     @IBAction func ClickButtonTwo(_ sender: Any) {
-         Bonnus.instance.tryToRedeemPrice(momento: "Login")
+         Bonnus.instance.tryToRedeemPrice(momento: "Momento2")
     }
 
     @IBAction func ClickButtonThree(_ sender: Any) {
-        Bonnus.instance.tryToRedeemPrice(momento: "Cuenta digital")
+        Bonnus.instance.tryToRedeemPrice(momento: "Momento3")
     }
 
     @IBAction func ClickButtonFour(_ sender: Any) {
-        Bonnus.instance.tryToRedeemPrice(momento: "Saldos")
+        Bonnus.instance.tryToRedeemPrice(momento: "Momento4")
     }
 
     @IBAction func ClickButtonFive(_ sender: Any) {
-        Bonnus.instance.tryToRedeemPrice(momento: "Transferencia")
+        Bonnus.instance.tryToRedeemPrice(momento: "Momento5ddf")
     }
 
     @IBAction func ClickButtonRewards(_ sender: Any) {
@@ -87,5 +88,14 @@ class DemoViewController: UIViewController {
             button.setTitleColor(secondColor, for: .normal)
         }
         
+    }
+}
+
+extension DemoViewController: cuponDelegate {
+    func didClickRedeemButton() {
+        print("Redeem Button Clicked")
+    }
+    func didClickSaveButton() {
+        print("CloseButton Clicked")
     }
 }
