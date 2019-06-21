825a768b2cddcffef03933921dc6cc242f6a6808
Emmanuel Casanas <emmanuel.casanas@globant.com>
2019-06-21 09:31:14 -0500

    Version 1.4.6
    Modal support example

--- Testing/ModalViewController.swift
+++ Testing/ModalViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 import BonnusSDK
 
 class ModalViewController: UIViewController {
+    
+    var isFirstTime: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
         Bonnus.instance.isModal = true
         Bonnus.instance.modalViewController = self
+        addButtonClose ()
     }
     
     @objc func buttonAction(sender: UIButton!) {
-       Bonnus.instance.tryToRedeemPrice(momento: "Momento2")
+        Bonnus.instance.tryToRedeemPrice(momento: "Momento2")
+    }
+    @objc func closeAction(sender: UIButton!) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        Bonnus.instance.tryToRedeemPrice(momento: "Momento2")
+        if isFirstTime {
+            Bonnus.instance.tryToRedeemPrice(momento: "Momento2")
+            isFirstTime = false
+        }
     }
     
     func addButton () {
@@ -32,6 +41,16 @@ class ModalViewController: UIViewController {
         button.backgroundColor = .green
         button.setTitle("Test Button", for: .normal)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    func addButtonClose () {
+        let button = UIButton(frame: CGRect(x: 0, y: 50, width: 100, height: 50))
+        button.backgroundColor = .white
+        button.setTitle("Close", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(closeAction), for: .touchUpInside)
         
         self.view.addSubview(button)
     }
